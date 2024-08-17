eda5e5021cd4ddc93445fa42fa5356ac62dea311
CONNEXGROW <100356697+CONNEXGROW@users.noreply.github.com>
2024-08-17 15:04:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
         hour = min * 60,
         day = hour * 24;
 
-  const end = new Date("August 18, 2024 00:00:00").getTime();
+  const end = new Date("August 19, 2024 00:00:00").getTime();
 
   const int = setInterval(() => {
     const current = new Date().getTime();
